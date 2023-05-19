c60795d1ed283fa5974c4a3526fce97dd6d33685
Manuel <manuharari@gmail.com>
2023-05-18 18:14:57 -0600

    HTML y CSS terminados; creando JS

---
+++ script.js
@@ -0,0 +1,5 @@
+var buttonEncriptar = document.querySelector(".button-encriptar");
+var buttonDesencriptar = document.querySelector(".button-desencriptar");
+var muneco = document.querySelector(".containerImage");
+var contenedor = document.querySelector(".containerParagraph");
+var result = document.querySelector(".resultext");
