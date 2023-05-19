e41687e9b01d1ceeb3b4dbfc0034e1ef2336df68
Manuel <manuharari@gmail.com>
2023-05-19 13:38:08 -0600

    Primera prueba nates de hacerlo responsivo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var buttonEncriptar = document.querySelector(".button-encriptar");
-var buttonDesencriptar = document.querySelector(".button-desencriptar");
+var buttonEncriptar = document.querySelector(".buttonEncriptar");
+var buttonDesencriptar = document.querySelector(".buttonDesencriptar");
 var muneco = document.querySelector(".containerImage");
 var container = document.querySelector(".containerParagraph");
 var result = document.querySelector(".resultext");
@@ -29,3 +29,72 @@ function ocultarAdelante(){
     container.classList.add("ocultar");
 }
 
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+for(var i = 0; i < texto.length; i++){
+    if(texto[i] == "a"){
+        textoFinal = textoFinal + "ai"
+    }
+    else if(texto[i] == "e"){
+        textoFinal = textoFinal + "enter"
+    }
+    else if(texto[i] == "i"){
+        textoFinal = textoFinal + "imes"
+    }
+    else if(texto[i] == "o"){
+        textoFinal = textoFinal + "ober"
+    }
+    else if(texto[i] == "u"){
+        textoFinal = textoFinal + "ufat"
+    }
+    else {
+        textoFinal = textoFinal + texto[i]
+    }
+}
+
+return textoFinal;
+
+}
+
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i= i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else {
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    
+    return textoFinal;
+    
+    }
+
+    const buttonCopiar = document.querySelector(".buttonCopiar");
+    buttonCopiar.addEventListener("click", copiar = () =>{
+        var contenido = document.querySelector(".textResult").textContent;
+        navigator.clipboard.writeText(contenido);
+        console.log("Listo");
+    })
