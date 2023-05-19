3da09f0c18c51b9f02c1cc3df0854990ea77f7ea
Manuel <manuharari@gmail.com>
2023-05-18 18:29:58 -0600

    Progreso en JS

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var buttonEncriptar = document.querySelector(".button-encriptar");
 var buttonDesencriptar = document.querySelector(".button-desencriptar");
 var muneco = document.querySelector(".containerImage");
-var contenedor = document.querySelector(".containerParagraph");
+var container = document.querySelector(".containerParagraph");
 var result = document.querySelector(".resultext");
+
+buttonEncriptar.onclick = encriptar;
+buttonDesencriptar.onclick = desencriptar;
+
+function encriptar(){
+    ocultarAdelante();
+    var textBox = recuperarTexto;
+    result.textContent = encriptarTexto(textBox)
+}
+
+function desencriptar(){
+    ocultarAdelante();
+    var textBox = recuperarTexto;
+    result.textContent = desencriptarTexto(textBox)
+}
